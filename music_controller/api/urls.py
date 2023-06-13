d241a7ae458963dea212823d77db4e4d106a63d6
lchester13 <leanna.r.chester@gmail.com>
2023-06-13 12:55:06 -0700

    creating room join page and home page

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -4,5 +4,7 @@ from . views import *
 urlpatterns = [
     path('room', RoomView.as_view() ),
     path('create-room', CreateRoomView.as_view()),
-    path('get-room', GetRoom.as_view() )
+    path('get-room', GetRoom.as_view() ),
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
 ]
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,16 +1,34 @@
 import { createRoot } from 'react-dom/client';
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
-import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography} from '@material-ui/core'
 
 
 export default function HomePage() {
+  const [roomCode, setRoomCode] = useState("")
+
+  useEffect(() => {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+      });
+  }, []);
+
     return (
         <Router>
           <Routes>
-            <Route path="/" element={<HomePageContent />} />
+            <Route exact path="/" element={
+            roomCode ? (
+              <Navigate to={`/room/${roomCode}`} />
+            ) : (
+              <HomePageContent />
+            )
+          }
+        />
             <Route path="/join" element={<RoomJoinPage />} />
             <Route path="/create" element={<CreateRoomPage />} />
             <Route path= "/room/:roomCode" element ={<Room />} exact />
@@ -20,7 +38,25 @@ export default function HomePage() {
 }
 
 function HomePageContent() {
-    return <p>This is the home page</p>;
+    return (
+      <Grid container spacing={3} align="center">
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup variant="contained" color="primary">
+            <Button color="primary" to='/join' component={ Link } >
+              Join a Room
+            </Button>
+            <Button color="secondary" to='/create' component={ Link } >
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    )
 }
 
 createRoot(document.getElementById('main')).render(<HomePage />);
